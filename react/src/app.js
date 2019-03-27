f27d012585a115979779b881804016d4d262f192
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-28 03:28:36 +0500

    Test router implmentation

--- react/src/app.js
+++ react/src/app.js
@@ -3,8 +3,29 @@ import React from 'react'
 import AppNav from './app/nav'
 
 
-export default (props) => {
-    return (<React.Fragment>
-        <AppNav { ... props.nav }/>
-    </React.Fragment>)
+export default class App extends React.Component {
+    renderRoute () {
+        const l = window.location
+        const { nav } = this.props
+        const c = nav.routes.find((e) => {
+            return e.path === l.pathname
+        })
+        console.log(l)
+        console.log(nav.routes)
+        console.log(c)
+        if (typeof c !== typeof undefined) {
+            return (props) => <c { ... props }/>
+        } else {
+            return () => null
+        }
+    }
+
+    render () {
+        const SelectedRoute = this.renderRoute()
+        return (<React.Fragment>
+            <AppNav { ... this.props.nav }/>
+            <SelectedRoute/>
+        </React.Fragment>)
+    }
+
 }
