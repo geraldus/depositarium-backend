9e102a8887bbd0893ab175bd5587f8b27f66d8f7
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-28 20:31:00 +0500

    Implement i18n JSON rep of Access Rights object

--- src/Local/Persist/Access.hs
+++ src/Local/Persist/Access.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Local.Persist.Access where
 
 import           ClassyPrelude.Yesod
@@ -14,9 +15,19 @@ data AccessType
     | UpdateUser
     | DeleteUser
     | SelfBalanceView
-    deriving (Generic, Show, Read, Eq, Enum, Bounded)
+    deriving (Generic, Show, Read, Eq, Ord, Enum, Bounded)
 derivePersistField "AccessType"
 
 instance ToJSON AccessType where
     toEncoding = genericToEncoding defaultOptions
 instance FromJSON AccessType
+
+data AccessRightJ = AccessRightJ
+    { accessRightJName  :: Text
+    , accessRightJTitle :: Text
+    , accessRightJDesc  :: Text }
+instance ToJSON AccessRightJ where
+    toJSON AccessRightJ{..} = object
+        [ "name"  .= toJSON accessRightJName
+        , "title" .= toJSON accessRightJTitle
+        , "desc"  .= toJSON accessRightJDesc ]
