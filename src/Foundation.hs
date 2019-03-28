56e3d5b715ccb289d1eafaaa4bcaf7dee6ce9e89
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-28 20:54:08 +0500

    Handle user list route

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -136,11 +136,12 @@ instance Yesod App where
         -> Bool       -- ^ Whether or not this is a "write" request.
         -> Handler AuthResult
     -- Routes not requiring authentication.
-    isAuthorized (AuthR _) _   = return Authorized
-    isAuthorized HomeR _       = return Authorized
-    isAuthorized FaviconR _    = return Authorized
-    isAuthorized RobotsR _     = return Authorized
-    isAuthorized (StaticR _) _ = return Authorized
+    isAuthorized (AuthR _) _         = return Authorized
+    isAuthorized HomeR _             = return Authorized
+    isAuthorized ManageListUserR _   = authorizeByAccess [ ListUsers ]
+    isAuthorized FaviconR _          = return Authorized
+    isAuthorized RobotsR _           = return Authorized
+    isAuthorized (StaticR _) _       = return Authorized
 
     -- This function creates static content files in the static folder
     -- and names them based on a hash of their content. This allows
