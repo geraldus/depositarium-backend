c239f6cdf95bf6087b390f724eaa592be485708f
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-30 17:40:00 +0500

    Define basic minimal user setup input form

---
+++ src/Form/UserData.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Form.UserData where
+
+import           Import
+import           Local.Persist.Access
+
+import           Type.UserData
+
+import           Data.Aeson           ( decode )
+import           Text.Read            ( readMaybe )
+
+
+userDataIForm :: Bool -> FormInput Handler UserData
+userDataIForm passwordless = UserData
+    <$> ireq textField "firstName"
+    <*> (orEmpty <$> iopt textField "patronymic")
+    <*> ireq textField "lastName"
+    <*> ireq textField "ident"
+    <*> ireq textField "email"
+    <*> (if passwordless then orEmpty <$> pass iopt else pass ireq)
+    <*> (jsonArray <$> ireq textField "rights")
+    where
+        pass o = o textField "password"
+        jsonArray :: Text -> [ AccessType ]
+        jsonArray =
+            catMaybes . mread . decode . encodeUtf8 . fromStrict
+
+        mread = maybe [] (map readMaybe)
+
+        orEmpty = fromMaybe ""
