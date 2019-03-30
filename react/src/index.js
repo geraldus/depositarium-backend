1088fa9b8a08a455af85cf09681e7ff097ddfbb8
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-30 19:57:46 +0500

    Impmenet create and update based on same form

--- react/src/index.js
+++ react/src/index.js
@@ -2,10 +2,13 @@ import App from './app.js'
 import HomePage from './pages/home'
 import ManageListUser from './pages/user-manager/list'
 import ManageCreateUser from './pages/user-manager/create'
+import ManageUpdateUser from './pages/user-manager/update'
 import OperatorProfileView from './pages/operator/profile/view'
+
 
 export const app = App
 export const homePage = HomePage
 export const manageListUser = ManageListUser
 export const manageCreateUser = ManageCreateUser
+export const manageUpdateUser = ManageUpdateUser
 export const operatorProfileView = OperatorProfileView
--- react/src/pages/user-manager/create.js
+++ react/src/pages/user-manager/create.js
@@ -1,71 +1,45 @@
 import React from 'react'
 import { defaultProps } from 'recompose'
 import _ from 'lodash'
+import 'whatwg-fetch'
 
-import FormGroup from '../../form/form-group'
-import FormCheck from '../../form/form-check'
+import UserForm from './form'
+import { setUserData } from './utils';
 
 export class CreateUser extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            name: '',
-            ident: '',
-            email: '',
-            password: '',
-            rights: []
+            user: {}
         }
     }
-    updateInputData (e, name) {
-
+    updateFormData (data) {
+        this.setState(s => {
+            let d = _.merge({}, s, { user: data })
+            // do not deep merge user rights array
+            d.rights = s.user.rights
+            return d
+        })
     }
-    createUser (e) {
-        e.preventDefault()
-        const formData = new FormData()
-        formData.append("name", this.state.name)
-        formData.append("ident", this.state.ident)
-        formData.append("email", this.state.email)
-        formData.append("password", this.state.password)
-        console.log(formData)
+    createUser (data) {
+        let formData = new FormData()
+        const user = data
+        setUserData(formData, user)
+        fetch(this.props.apiUrl, {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            console.log(response)
+            response.json()
+        }).then(json => console.log)
     }
     render () {
-        const L = this.props.labels
-        const { id, accessRights } = this.props
-        return (<React.Fragment>
-            <div className="row">
-                <div className="col">
-                    <FormGroup
-                        name="name"
-                        label={L.nameTitle}
-                        value={this.state.name}/>
-                    <FormGroup
-                        name="ident"
-                        label={L.identTitle}
-                        value={this.state.ident}/>
-                    <FormGroup
-                        name="email"
-                        label={L.emailTitle}
-                        value={this.state.email}/>
-                    <FormGroup
-                        name="password"
-                        label={L.passwordTitle}
-                        value={this.state.password}/>
-                    {accessRights.map((ar, idx) =>
-                        <FormCheck
-                            key={idx}
-                            type="checkbox"
-                            name="rights"
-                            label={ar.name}
-                            value={ar.name}
-                            />)}
-                    <button
-                        className="btn btn-success"
-                        onClick={e => this.createUser(e)}>
-                        {L.save}
-                        </button>
-                </div>
-            </div>
-        </React.Fragment>)
+        return (<UserForm
+                onChange={data => this.updateFormData(data)}
+                onSubmit={data => this.createUser(data)}
+                { ... this.props.user }
+                accessRights={this.props.accessRights}
+                />)
     }
 }
 
