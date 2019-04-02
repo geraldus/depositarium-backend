c45fe108394c71648e6984c801cab5b0f1dfe68f
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-02 14:31:04 +0500

    Improve Rollup config
    Get rid of warning by excluding `node_modules` folder from babel plugin
    Configure uglification in production
    Update package command

--- react/rollup.config.js
+++ react/rollup.config.js
@@ -1,18 +1,21 @@
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs';
-import { uglify } from 'rollup-plugin-uglify';
-import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+// import postcss from 'rollup-plugin-postcss'
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
-const production = !process.env.ROLLUP_WATCH;
+
+const production = !process.env.ROLLUP_WATCH
 
 const plugins = [
     resolve(), // tells Rollup how to find date-fns in node_modules
     commonjs({
         include: 'node_modules/**',
         namedExports: {
+            // 'src/bs4/index.js': [ 'Bs4Containers' ],
             'node_modules/react/index.js': [
                 'Component',
                 'PureComponent',
@@ -26,11 +29,13 @@ const plugins = [
             ]
         }
     }), // converts date-fns to ES modules
-    babel(),
+    babel({
+        exclude: 'node_modules/**'
+    }),
     replace({
         'process.env.NODE_ENV': JSON.stringify( 'production' )
     }),
-    production && uglify() // minify, but only in production
+    production && terser() // minify, but only in production
 ]
 
 export default [
@@ -38,8 +43,10 @@ export default [
         input: 'src/index.js',
         output: {
             file: '../static/js/bundle.js',
-            format: 'umd', // immediately-invoked function expression — suitable for <script> tags
-            // format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+            format: 'umd',
+            // format: 'iife',
+            // immediately-invoked function expression
+            // suitable for <script> tags
             sourcemap: false,
             name: "_ui"
         },
