45978af064f94468a8f88e4f1eaa5518fb4f0095
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-02 14:31:40 +0500

    Code clean up

--- src/Utils/Common.hs
+++ src/Utils/Common.hs
@@ -1,19 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
 {-# LANGUAGE TypeFamilies      #-}
 module Utils.Common where
 
 import           Import.NoFoundation
 
-import           Utils.Time
 
 import           Data.Aeson          ( encode )
 import qualified Data.HashMap.Lazy   as HML
-import           Data.Time.Format    ( TimeLocale (..) )
 import           Text.Julius         ( RawJS (..), RawJavascript )
 
 
-{- YESOD.  APP -}
+-- * Yesod
 
 setCompositeTitle
     :: (MonadWidget m, WidgetFor site ~ m, RenderMessage site msg)
@@ -30,7 +27,6 @@ txt = pack . show
 
 -- *** JSON
 
--- encodeMap :: (Functor f, ToJSON a) => f a -> [a] -> a
 rawEncodeMap
     :: (ToJSON (f b), Functor f)
     => (a -> b) -> f a -> RawJavascript
@@ -59,37 +55,3 @@ errorResponseWithDataJ
     :: Text -> [(Text, Value)] -> Value
 errorResponseWithDataJ errorText =
     jsonMerge . (:) (errorResponseJ errorText) . (:[]) . object
-
-
-
-{- TIME -}
-
-dateTimeRowWM :: (WidgetFor s ~ m, MonadWidget m) => UTCTime -> m ()
-dateTimeRowWM t = do
-    fd <- getFormatDateRender
-    ft <- getFormatTimeRender
-    [whamlet|
-        <span .text-uppercase>#{fd t}#
-        &nbsp;&nbsp;#
-        <span .text-muted>#{ft t}|]
-
-getFormatDateRender :: MonadWidget m => m (UTCTime -> Html)
-getFormatDateRender = (\(l, t) -> localeFormatDate l . offsetTime t)
-    <$> getFormatParams
-
-getFormatTimeRender :: MonadWidget m => m (UTCTime -> Html)
-getFormatTimeRender = (\(l, t) -> localeFormatTime l . offsetTime t)
-    <$> getFormatParams
-
-getFormatParams :: MonadWidget m => m (TimeLocale, Int)
-getFormatParams = (,)
-    <$> liftHandler selectLocale
-    <*> liftHandler timezoneOffsetFromCookie
-
-selectLocale :: MonadHandler m => m TimeLocale
-selectLocale = locale <$> languages
-    where
-    locale ("ru":_) = ruTimeLocale
-    locale (_:rest) = locale rest
-    locale []       = defaultTimeLocale
-
