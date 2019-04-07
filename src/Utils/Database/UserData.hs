8de91eecffbbe822041f810fabf4315fd4bee43c
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-07 22:12:15 +0500

    Generalize user metadata fetch

--- src/Utils/Database/UserData.hs
+++ src/Utils/Database/UserData.hs
@@ -4,39 +4,53 @@
 {-# LANGUAGE RankNTypes        #-}
 module Utils.Database.UserData where
 
-import           Import.NoFoundation as I hiding ( on, (==.) )
+import           Import.NoFoundation as I hiding ( on, (==.), (||.) )
 import           Utils.Common        ( jsonMerge )
 
 import           Data.Aeson          as A
 import           Database.Esqueleto
 
 
--- getUserMetaData :: UserId -> (Entity User, Maybe (Entity Email), Maybe (Entity UserMeta), [AccessType])
+type MaybeEntity a = Maybe (Entity a)
+
+type UserMetas = (Entity User, MaybeEntity Email, MaybeEntity UserMeta, [Entity UserRights])
+
+
+
 getUserMetaData ::
-    forall (m :: * -> *) backend.
-    (MonadIO m, BackendCompatible SqlBackend backend,
-    PersistQueryRead backend, PersistUniqueRead backend)
-    => Key User
-    -> ReaderT backend m
-        (Maybe
-            ( Entity User
-            , Maybe (Entity Email)
-            , Maybe (Entity UserMeta)
-            , [Entity UserRights] ))
-getUserMetaData user = do
+    ( MonadIO m
+    , BackendCompatible SqlBackend backend
+    , PersistQueryRead backend
+    , PersistUniqueRead backend)
+    => Key User -> ReaderT backend m (Maybe UserMetas)
+getUserMetaData = getUserMetaDataEither . Left
+
+getUserMetaDataEither ::
+    ( MonadIO m
+    , BackendCompatible SqlBackend backend
+    , PersistQueryRead backend
+    , PersistUniqueRead backend)
+    => Either (Key User) Text -> ReaderT backend m (Maybe UserMetas)
+getUserMetaDataEither user = do
+    let whereCondition u e = case user of
+            Left userId -> u ^. UserId ==. val userId
+            Right userIdent ->
+                u ^. UserIdent ==. val userIdent
+                ||. e ?. EmailEmail ==. just (val userIdent)
     meta <- select . from $ \(u `LeftOuterJoin` e `LeftOuterJoin` m) -> do
         on (just (u ^. UserId) ==. m ?. UserMetaUser)
         on (just (u ^. UserId) ==. e ?. EmailUser)
-        where_ (u ^. UserId ==. val user)
+        where_ (whereCondition u e)
         return (u, e, m)
     withExistingUser meta $ \(u, e, m) -> do
         access <- select . from $ \a -> do
-            where_ (a ^. UserRightsUser ==. val user)
+            where_ (a ^. UserRightsUser ==. val (entityKey u))
             return a
         return (u, e, m, access)
     where
         withExistingUser [] _         = pure Nothing
         withExistingUser (m:_) action = Just <$> action m
+
 
 
 cleanJSONUserData ::
